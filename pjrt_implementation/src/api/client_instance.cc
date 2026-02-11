0a3cb92c7ca1ac8a99196feae2a04f1dad01b982
Muhammad Asif Manzoor <mmanzoor@tenstorrent.com>
2026-02-11 17:30:22 +0000

    flush debug logs

--- pjrt_implementation/src/api/client_instance.cc
+++ pjrt_implementation/src/api/client_instance.cc
@@ -192,6 +192,7 @@ ClientInstance::~ClientInstance() {
   DLOG_F(LOG_DEBUG, "ClientInstance::~ClientInstance");
   closeMeshDevice();
   std::remove(m_cached_system_descriptor_path.data());
+  loguru::flush();
 }
 
 PJRT_Error *ClientInstance::initialize() {
